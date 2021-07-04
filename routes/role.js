8f45e399f31821ec792c047acda2b1b4a5efd561
Sujal Lama <lsujal41@gmail.com>
2021-07-04 16:59:59 +0545

    protect and permit role

--- routes/role.js
+++ routes/role.js
@@ -2,15 +2,17 @@ const { getAllRoles, createRole, updateRole,
     deleteRole, getSingleRole} = require('../controllers/role');
 
 const router = require('express').Router();
+const protect = require('../middleware/authMiddleware');
+const permit = require('../middleware/permissionAuth');
 
 router.route('/')
     .get(getAllRoles)
-    .post( createRole);
+    .post(protect, permit('admin'), createRole);
 
 router.route('/:id')
     .get(getSingleRole)
-    .put( updateRole)
-    .delete(deleteRole);
+    .put(protect, permit('admin'), updateRole)
+    .delete(protect, permit('admin'), deleteRole);
     
 
 module.exports = router
